6ace89d3cadbcdba0dedf9e9c25e162d303496da
Alundy <alundy@ntia.doc.gov>
2017-08-31 09:53:14 -0400

    Added Qterm, the inverse complementary normal distribution function

--- ITUClutterLoss/ClutterLoss.cs
+++ ITUClutterLoss/ClutterLoss.cs
@@ -6,23 +6,39 @@ using System.Threading.Tasks;
 
 namespace ITUClutterLoss
 {
+    /// <summary>
+    /// ITU-R P.2108-0
+    /// </summary>
     public class ClutterLoss
     {
         public Random rando { get; set; }
 
         public double Nnormaldist { get; set; }
 
+        public double Qterm;
+
+        public double LldB;
+
+        public double LsdB;
+
         public ClutterLoss(Random rando)
         {
             this.rando = rando;
+            
         }
-
+        /// <summary>
+        /// ITU-R P.2108-0 Section 3.2. Designed for input P to be random and output Qterm is random
+        /// </summary>
+        /// <param name="FrequencyGHz"></param>
+        /// <param name="Distancekm"></param>
+        /// <returns>Clutter in Terrestrial Link</returns>
         public double TerrestrialtoTerrestrialClutterLoss(double FrequencyGHz, double Distancekm)
         {
-            double LldB = 23.5 + 9.6 * Math.Log10(FrequencyGHz);
-            double LsdB = 32.98 + 23.9 * Math.Log10(Distancekm) + 3 * Math.Log10(FrequencyGHz);
-            Nnormaldist = N(0, 6);
-            double ClutterLossdB = -5 * Math.Log10(Math.Pow(10, -0.2 * LldB) + Math.Pow(10, -0.2 * LsdB));
+            LldB = 23.5 + 9.6 * Math.Log10(FrequencyGHz);
+            LsdB = 32.98 + 23.9 * Math.Log10(Distancekm) + 3 * Math.Log10(FrequencyGHz);
+            Nnormaldist = N(0, 1);
+            Qterm = 6 * Nnormaldist;
+            double ClutterLossdB = -5 * Math.Log10(Math.Pow(10, -0.2 * LldB) + Math.Pow(10, -0.2 * LsdB)) - Qterm;
 
             return ClutterLossdB;
         }
